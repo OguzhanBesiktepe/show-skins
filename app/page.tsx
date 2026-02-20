7f89f6bd3c8376f4386113a7e00b50c395555d5e
OguzhanBesiktepe <oguzhanbesiktepe@gmail.com>
2026-02-19 21:36:12 -0500

    Fetched Data from CSGO-API. Successful result. Created pickRandom function to sort through 8 random skins from list. Displayed on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,48 @@
-//Homepage
 import SkinCard from "./components/SkinCard";
 
 async function getSkins() {
   const res = await fetch(
     "https://raw.githubusercontent.com/ByMykel/CSGO-API/main/public/api/en/skins_not_grouped.json",
+    { cache: "no-store" },
   );
 
   return res.json();
 }
 
-export default function Home() {
+export default async function Home() {
+  const skins = await getSkins();
+
+  //Select random 8 skins from API List without duplicates
+
+  function pickRandom<T>(arr: T[], n: number) {
+    const copy = [...arr];
+    for (let i = copy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [copy[i], copy[j]] = [copy[j], copy[i]];
+    }
+    return copy.slice(0, n);
+  }
+
+  const firstSkins = pickRandom(skins, 8);
+
   return (
     <main className="min-h-screen bg-zinc-950 text-white">
       <div className="max-w-7xl mx-auto px-6 py-10">
         <h1 className="text-3xl font-bold">ShowSkins</h1>
-        <p className="mt-2 text-zinc-400">Card layout prototype.</p>
 
         <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-          <SkinCard
-            weapon="MP9"
-            name="Starlight Protector"
-            rarityLabel="Covert SMG"
-            rarityColor="#eb4b4b"
-            hasStatTrak={true}
-            imageUrl="https://community.fastly.steamstatic.com/economy/image/i0CoZ81Ui0m-9KwlBY1L_18myuGuq1wfhWSaZgMttyVfPaERSR0Wqmu7LAocGIGz3UqlXOLrxM-vMGmW8VNxu5Dx60noTyL8js_f-jFk4uL3V7d5IeKfB2CY1dF7teVgWiT9wU0htTjWnI2qcHvEZgQlW5VyROAD50W6lYDnN-zi5QyM2YtGzir43zQJsHh8IziyOQ/330x192?allow_animated=1"
-            priceRange="$68.80 - $106.43"
-            statTrakPriceRange="$62.58 - $94.98"
-            sourceLabel="Dreams & Nightmares Case"
-            sourceImageUrl="https://community.fastly.steamstatic.com/economy/image/i0CoZ81Ui0m-9KwlBY1L_18myuGuq1wfhWSaZgMttyVfPaERSR0Wqmu7LAocGJKz2lu_XsnXwtmkJjSU91dh8bj35VTqVBP4io_frnIV7Kb5OaU-JqfHDzXFle0u4LY8Gy_kkRgisGzcm4v4J3vDOAQmDMdyRvlK7EcmeCU3yw/330x192?allow_animated=1"
-          />
+          {firstSkins.map((skin: any) => (
+            <SkinCard
+              key={skin.id}
+              weapon={skin.weapon?.name ?? "Unknown"}
+              name={skin.name}
+              rarityLabel={skin.rarity?.name ?? "Unknown"}
+              rarityColor={skin.rarity?.color ?? "#888888"}
+              hasStatTrak={skin.stattrak ?? false}
+              imageUrl={skin.image}
+              sourceLabel={skin.collection?.name}
+            />
+          ))}
         </div>
       </div>
     </main>
