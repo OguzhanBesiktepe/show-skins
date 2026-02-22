87eb7e78300f5e39e881eda1034064730a4ab3ae
OguzhanBesiktepe <oguzhanbesiktepe@gmail.com>
2026-02-22 12:20:40 -0500

    added full navbar dropdown support for all the rest of the weapon categories, fetched using the CSGO API.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,9 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 import NavDropdown from "./NavDropdown";
 
-const navLink =
-  "flex items-center gap-1 text-base font-semibold tracking-wide text-zinc-200 hover:text-amber-400 transition-colors";
-
 const pistols = [
   { label: "CZ75-Auto", slug: "cz75-auto" },
   { label: "Desert Eagle", slug: "desert-eagle" },
@@ -17,6 +14,74 @@ const pistols = [
   { label: "Tec-9", slug: "tec-9" },
   { label: "USP-S", slug: "usp-s" },
   { label: "Zeus x27", slug: "zeus-x27" },
+];
+
+const smgs = [
+  { label: "MAC-10", slug: "mac-10" },
+  { label: "MP5-SD", slug: "mp5-sd" },
+  { label: "MP7", slug: "mp7" },
+  { label: "MP9", slug: "mp9" },
+  { label: "P90", slug: "p90" },
+  { label: "PP-Bizon", slug: "pp-bizon" },
+  { label: "UMP-45", slug: "ump-45" },
+];
+
+const lmgs = [
+  { label: "M249", slug: "m249" },
+  { label: "Negev", slug: "negev" },
+];
+
+const shotguns = [
+  { label: "MAG-7", slug: "mag-7" },
+  { label: "Nova", slug: "nova" },
+  { label: "Sawed-Off", slug: "sawed-off" },
+  { label: "XM1014", slug: "xm1014" },
+];
+
+const rifles = [
+  { label: "AK-47", slug: "ak-47" },
+  { label: "AUG", slug: "aug" },
+  { label: "AWP", slug: "awp" },
+  { label: "FAMAS", slug: "famas" },
+  { label: "G3SG1", slug: "g3sg1" },
+  { label: "Galil AR", slug: "galil-ar" },
+  { label: "M4A1-S", slug: "m4a1-s" },
+  { label: "M4A4", slug: "m4a4" },
+  { label: "SCAR-20", slug: "scar-20" },
+  { label: "SG 553", slug: "sg-553" },
+  { label: "SSG 08", slug: "ssg-08" },
+];
+
+const knives = [
+  { label: "Bayonet", slug: "bayonet" },
+  { label: "Bowie Knife", slug: "bowie-knife" },
+  { label: "Butterfly Knife", slug: "butterfly-knife" },
+  { label: "Falchion Knife", slug: "falchion-knife" },
+  { label: "Flip Knife", slug: "flip-knife" },
+  { label: "Gut Knife", slug: "gut-knife" },
+  { label: "Huntsman Knife", slug: "huntsman-knife" },
+  { label: "Karambit", slug: "karambit" },
+  { label: "M9 Bayonet", slug: "m9-bayonet" },
+  { label: "Navaja Knife", slug: "navaja-knife" },
+  { label: "Nomad Knife", slug: "nomad-knife" },
+  { label: "Paracord Knife", slug: "paracord-knife" },
+  { label: "Shadow Daggers", slug: "shadow-daggers" },
+  { label: "Skeleton Knife", slug: "skeleton-knife" },
+  { label: "Stiletto Knife", slug: "stiletto-knife" },
+  { label: "Survival Knife", slug: "survival-knife" },
+  { label: "Talon Knife", slug: "talon-knife" },
+  { label: "Ursus Knife", slug: "ursus-knife" },
+];
+
+const gloves = [
+  { label: "Bloodhound Gloves", slug: "bloodhound-gloves" },
+  { label: "Broken Fang Gloves", slug: "broken-fang-gloves" },
+  { label: "Driver Gloves", slug: "driver-gloves" },
+  { label: "Hand Wraps", slug: "hand-wraps" },
+  { label: "Hydra Gloves", slug: "hydra-gloves" },
+  { label: "Moto Gloves", slug: "moto-gloves" },
+  { label: "Specialist Gloves", slug: "specialist-gloves" },
+  { label: "Sport Gloves", slug: "sport-gloves" },
 ];
 
 export default function Navbar() {
@@ -38,30 +103,16 @@ export default function Navbar() {
         {/* Navigation */}
         <div className="flex items-center gap-8">
           <NavDropdown title="Pistols" href="/browse/pistols" items={pistols} />
-
-          <Link className={navLink} href="/browse/SMGs">
-            SMGs <span className="text-zinc-400">▾</span>
-          </Link>
-
-          <Link className={navLink} href="/browse/LMGs">
-            LMGs <span className="text-zinc-400">▾</span>
-          </Link>
-
-          <Link className={navLink} href="/browse/shotguns">
-            Shotguns <span className="text-zinc-400">▾</span>
-          </Link>
-
-          <Link className={navLink} href="/browse/rifles">
-            Rifles <span className="text-zinc-400">▾</span>
-          </Link>
-
-          <Link className={navLink} href="/browse/knives">
-            Knives <span className="text-zinc-400">▾</span>
-          </Link>
-
-          <Link className={navLink} href="/browse/gloves">
-            Gloves <span className="text-zinc-400">▾</span>
-          </Link>
+          <NavDropdown title="SMGs" href="/browse/smgs" items={smgs} />
+          <NavDropdown title="LMGs" href="/browse/lmgs" items={lmgs} />
+          <NavDropdown
+            title="Shotguns"
+            href="/browse/shotguns"
+            items={shotguns}
+          />
+          <NavDropdown title="Rifles" href="/browse/rifles" items={rifles} />
+          <NavDropdown title="Knives" href="/browse/knives" items={knives} />
+          <NavDropdown title="Gloves" href="/browse/gloves" items={gloves} />
         </div>
 
         {/* Spacer */}
