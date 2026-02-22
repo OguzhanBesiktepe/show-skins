f9b16408dfd101b4aa8cdfc1c13f6640f2b56422
OguzhanBesiktepe <oguzhanbesiktepe@gmail.com>
2026-02-22 12:13:32 -0500

    Still debating on color scheme of site. Added counterstrike style font for homepage, and weapon pages.

--- app/browse/[category]/[weapon]/page.tsx
+++ app/browse/[category]/[weapon]/page.tsx
@@ -90,7 +90,9 @@ export default async function WeaponPage({
   return (
     <main className="min-h-screen text-white">
       <div className="max-w-7xl mx-auto px-6 py-10">
-        <h1 className="text-3xl font-bold">{weaponName} Skins</h1>
+        <h1 className="text-3xl font-bold" style={{ fontFamily: "CS Regular" }}>
+          {weaponName} Skins
+        </h1>
 
         <p className="mt-2 text-zinc-400">
           Showing {from} - {to}
--- app/page.tsx
+++ app/page.tsx
@@ -58,6 +58,13 @@ export default async function Home({
   return (
     <main className="min-h-screen text-white">
       <div className="max-w-7xl mx-auto px-6 py-10">
+        <h1
+          className="text-2xl sm:text-3xl font-bold"
+          style={{ fontFamily: "CS Regular" }}
+        >
+          Browse All Counter Strike Skins
+        </h1>
+
         <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {pageItems.map((skin) => (
             <SkinCard
